8c0fd75d15c96175113484b40cb32c8b883cce36
Christopher Kyle Horton <christhehorton@gmail.com>
2023-01-23 00:13:10 -0500

    test: add D6 tests

---
+++ Xyaneon.Games.Dice.Test/D6Test.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xyaneon.Games.Dice.Test
+{
+    [TestClass]
+    public class D6Test
+    {
+        [TestMethod]
+        public void Constructor_Default_ShouldInitializeFaceCountTo6()
+        {
+            var die = new D6();
+
+            Assert.AreEqual(6, die.FaceCount);
+        }
+
+        [TestMethod]
+        public void Constructor_Seed_ShouldInitializeFaceCountTo4()
+        {
+            var die = new D6(123);
+
+            Assert.AreEqual(6, die.FaceCount);
+        }
+    }
+}
